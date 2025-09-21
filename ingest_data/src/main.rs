efff1a21e2ffe04360d437a20024bb210dd97e9c
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-09-21 21:54:52 +0200

    Move util functionality to util module

--- ingest_data/src/main.rs
+++ ingest_data/src/main.rs
@@ -1,49 +1,53 @@
+mod util;
+
 use std::collections::HashMap;
 
-use arrow::compute;
-use arrow::datatypes::{ArrowPrimitiveType, DataType, Field, Schema};
 use arrow::record_batch::RecordBatch;
-use datafusion::parquet::arrow::ParquetRecordBatchStreamBuilder;
-use futures_util::TryStreamExt;
 use modelardb_embedded::TableType;
 use modelardb_embedded::operations::Operations;
 use modelardb_embedded::operations::client::{Client, Node};
-use modelardb_types::types::{ArrowTimestamp, ArrowValue};
 
 async fn create_table(mut modelardb_client: Client) {
-    let schema = Schema::new(vec![
-        Field::new("timestamp", ArrowTimestamp::DATA_TYPE, false),
-        Field::new("park_id", DataType::Utf8, false),
-        Field::new("windmill_id", DataType::Utf8, false),
-        Field::new("wind_speed", ArrowValue::DATA_TYPE, false),
-        Field::new("pitch_angle", ArrowValue::DATA_TYPE, false),
-        Field::new("rotor_speed", ArrowValue::DATA_TYPE, false),
-        Field::new("active_power", ArrowValue::DATA_TYPE, false),
-        Field::new("cos_nacelle_dir", ArrowValue::DATA_TYPE, false),
-        Field::new("sin_nacelle_dir", ArrowValue::DATA_TYPE, false),
-        Field::new("cos_wind_dir", ArrowValue::DATA_TYPE, false),
-        Field::new("sin_wind_dir", ArrowValue::DATA_TYPE, false),
-        Field::new("cor_nacelle_direction", ArrowValue::DATA_TYPE, false),
-        Field::new("cor_wind_direction", ArrowValue::DATA_TYPE, false),
-    ]);
-
+    let schema = util::table_schema();
     let table_type = TableType::TimeSeriesTable(schema, HashMap::new(), HashMap::new());
 
-    modelardb_client
-        .create("wind", table_type)
-        .await
-        .unwrap();
+    modelardb_client.create("wind", table_type).await.unwrap();
 }
 
-fn main() {
-    println!("Hello, world!");
-async fn read_wind_data() -> RecordBatch {
-    let file = tokio::fs::File::open("data/wind_cleaned.parquet").await.unwrap();
-    let builder = ParquetRecordBatchStreamBuilder::new(file).await.unwrap();
+async fn ingest_into_table_task(
+    mut modelardb_client: Client,
+    wind_data: RecordBatch,
+    park_id: &str,
+    windmill_id: &str,
+) {
+    let num_rows = wind_data.num_rows();
+    let batch_size = 1000;
+    let mut start = 0;
 
-    let stream = builder.build().unwrap();
-    let record_batches = stream.try_collect::<Vec<_>>().await.unwrap();
+    loop {
+        let end = if start + batch_size > num_rows {
+            num_rows
+        } else {
+            start + batch_size
+        };
 
-    compute::concat_batches(&record_batches[0].schema(), &record_batches).unwrap()
+        let slice = wind_data.slice(start, end - start);
+        let time_series = util::generate_time_series_data(slice, park_id, windmill_id).await;
+
+        start = end % num_rows;
+    }
 }
+
+#[tokio::main]
+async fn main() {
+    // Connect to the two ModelarDB edge nodes and create the table.
+    let edge_1 = Node::Server("grpc://host.docker.internal:9991".to_owned());
+    let edge_1_client = Client::connect(edge_1).await.unwrap();
+    create_table(edge_1_client).await;
+
+    let edge_2 = Node::Server("grpc://host.docker.internal:9992".to_owned());
+    let edge_2_client = Client::connect(edge_2).await.unwrap();
+    create_table(edge_2_client).await;
+
+    let wind_data = util::read_wind_data().await;
 }
