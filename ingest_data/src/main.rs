551c1f9bc476265ed6bcf902dfd07dabfa7c999e
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-09-21 20:58:52 +0200

    Add method to create table for a client

--- ingest_data/src/main.rs
+++ ingest_data/src/main.rs
@@ -1,3 +1,36 @@
+use std::collections::HashMap;
+
+use arrow::datatypes::{ArrowPrimitiveType, DataType, Field, Schema};
+use modelardb_embedded::TableType;
+use modelardb_embedded::operations::Operations;
+use modelardb_embedded::operations::client::Client;
+use modelardb_types::types::{ArrowTimestamp, ArrowValue};
+
+async fn create_table(mut modelardb_client: Client) {
+    let schema = Schema::new(vec![
+        Field::new("timestamp", ArrowTimestamp::DATA_TYPE, false),
+        Field::new("park_id", DataType::Utf8, false),
+        Field::new("windmill_id", DataType::Utf8, false),
+        Field::new("wind_speed", ArrowValue::DATA_TYPE, false),
+        Field::new("pitch_angle", ArrowValue::DATA_TYPE, false),
+        Field::new("rotor_speed", ArrowValue::DATA_TYPE, false),
+        Field::new("active_power", ArrowValue::DATA_TYPE, false),
+        Field::new("cos_nacelle_dir", ArrowValue::DATA_TYPE, false),
+        Field::new("sin_nacelle_dir", ArrowValue::DATA_TYPE, false),
+        Field::new("cos_wind_dir", ArrowValue::DATA_TYPE, false),
+        Field::new("sin_wind_dir", ArrowValue::DATA_TYPE, false),
+        Field::new("cor_nacelle_direction", ArrowValue::DATA_TYPE, false),
+        Field::new("cor_wind_direction", ArrowValue::DATA_TYPE, false),
+    ]);
+
+    let table_type = TableType::TimeSeriesTable(schema, HashMap::new(), HashMap::new());
+
+    modelardb_client
+        .create("wind", table_type)
+        .await
+        .unwrap();
+}
+
 fn main() {
     println!("Hello, world!");
 }
