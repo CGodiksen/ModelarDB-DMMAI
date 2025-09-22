7b92646c899d3ed1a66719b64e52b3da743aa54d
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-09-22 19:43:48 +0200

    Add Python file that curates the data and saves it locally

--- curate_data/main.py
+++ curate_data/main.py
@@ -1,2 +1,18 @@
+from pathlib import Path
+
+import modelardb
+
 if __name__ == "__main__":
-    print("Hello World")
+    # Connect to a cloud node.
+    modelardbd_cloud = modelardb.connect(modelardb.Server("grpc://host.docker.internal:9999"))
+
+    # Execute a query to retrieve data from the "wind" table.
+    record_batch = modelardbd_cloud.read("SELECT * FROM wind LIMIT 100")
+    df = record_batch.to_pandas()
+
+    # Save the DataFrame to a CSV file.
+    output_file = "wind.csv"
+    output_dir = Path("datasets")
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+    df.to_csv(output_dir / output_file, index=False)
